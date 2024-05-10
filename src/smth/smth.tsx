f26f832fe6b93dd08b2f89fdb9c4c494c7f469ba
Liza <fildepiorsta@gmail.com>
2024-05-10 15:38:16 +0300

    i got it

--- src/smth/smth.tsx
+++ src/smth/smth.tsx
@@ -1,23 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Smth = () => {
-    let breeds: any = [];
-  
-  useEffect(()=> {
-       fetch('https://api.thecatapi.com/v1/breeds')
-    .then(response=> response.json())
-    .then(data=>{
-      breeds = data; 
-      console.log(data)})
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.thecatapi.com/v1/breeds");
+        const data = await response.json();
+        setBreeds(data);
+        console.log(breeds);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
     
-  }, [])
+    }
+   fetchData(); }, []);
+
   return (
-        <div>
-           <label htmlFor="breed-select">Choose a pet:</label>
-           {breeds.length !==0 && 
-           <select  name="pets" id="breed-select"> 
-       
-           {breeds.map((breed:any) => <option value={breed.id}>{breed.name}</option>)}
-           </select>}
-        </div>
-    )}
+    <div>
+      <label htmlFor="breed-select">Choose a pet:</label>
+      {breeds.length !== 0 && (
+        <select name="pets" id="breed-select">
+          {breeds.map((breed: any) => (
+            <option value={breed.id}>{breed.name}</option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+};
