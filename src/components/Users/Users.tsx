85411d94841a5e94c9fa25ad9ca0708a3142877a
Liza <fildepiorsta@gmail.com>
2024-05-12 16:14:02 +0300

    xml version

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,10 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-export const Users = (props: any) => {
-  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    // const fetchData = async () => {
+ // const fetchData = async () => {
     //   try {
     //     const response = await fetch('https://social-network.samuraijs.com/api/1.0/users');
     //     const data = await response.json();
@@ -12,19 +9,23 @@ export const Users = (props: any) => {
     //     setUsers(data.items);
     //   } catch (error) {
     //     console.error('Error fetching data:', error);
-    //   }
-    const fetchData = async () => {
+    //   }}
+
+export const Users = (props: any) => {
+  const [users, setUsers] = useState([]);
+  const fetchData = async () => {
       try {
         const data = await axios.get(
           "https://social-network.samuraijs.com/api/1.0/users"
         );
 
         setUsers(data.data.items);
-      } catch (e) {
-        console.log(e);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
     };
 
+  useEffect(() => {
     fetchData();
   }, []);
 
